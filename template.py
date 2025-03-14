c356fc9fb082ce9e08d91d485f6662dbf0679446
Shreya-Nayak <ayshre2478@gmail.com>
2025-03-14 23:08:39 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,53 @@
+#TEMPLATE FOR FILES AND FOLDERS
+
+# os and pathlib help manage folders & files dynamically.
+# log : it keeps track of what happens while the code runs.
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    # .github to directly deploy in cloud from github
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",#constructor file (local package)
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "congig/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath) 
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file {filename}")
+
+        #if code is there or any text in the file then size is not zero that means replace if file is empty else don't
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creatinf empty file: {filepath}")
+
+
+    else:
+                    logging.info(f"{filename} already exists")
