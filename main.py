13d4fb836da60737c139e455d4bd901cc7056480
Shreya-Nayak <ayshre2478@gmail.com>
2025-03-15 20:18:07 +0530

    validation of data

--- main.py
+++ main.py
@@ -8,15 +8,28 @@ project_root = Path(r"C:\Users\ayshr\OneDrive\Desktop\Replit\Text-Summarizer")
 os.chdir(project_root)
 
 from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
-
+from textSummarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
-STAGE_NAME = "Data Ingestion stage"
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+# STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
+
